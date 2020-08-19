9e7becd7eb4d7a0ae812836d4275b17b79fd11d5
NathanielGaffney <nate440@gmail.com>
2020-08-19 02:25:45 -0400

    wireframe and function stubs

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import './index.css';
+
+//upon start
+//get values from api
+//store locally
+//render
+
+//get form values
+//api call
+//same vales local object
+//render
+
+function filterButton(){
+    //when you click the filter button, the filter section displays
+}
+
+function filterSubmit(){
+    //when you click the filter submit button, the value in the
+    //text input is set to display only bookmarks with a rating value
+    //that is >= the input
+    //items not passing the check are hidden
+    //render
+}
+
+function addButton(){
+    //when you click add button, the new section displays
+}
+
+function addSubmit(){
+    //when 'save bookmark' is clicked, an api call takes the input of the form
+    //and stores an object to the server
+    //if no error, then the same values are used to create a local object
+    //render
+}
+
+function itemButton(){
+    //when the area is clicked reverse the hidden state 
+    //of the description for that item
+}
+
+function editButton (){
+    //when the button is clicked, hide the description and title
+    //un-hide the edit box
+}
+
+function editSubmit(){
+    //when the edit is submitted, the information in the forms are put into
+    //an api call, which patches an existing object at the server
+    //the same values are used to update the existing local object
+    //render
+}
